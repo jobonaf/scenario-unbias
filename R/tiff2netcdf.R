e766394115a863a7a76afd0a02bf963ff117f184
Giovanni Bonafè <midable@gmail.com>
2026-04-29 14:44:16 +0200

    added skill_scores_summary.R; tiff2netcdf.R adapted to phase 2

--- R/tiff2netcdf.R
+++ R/tiff2netcdf.R
@@ -82,22 +82,54 @@ tiff_to_netcdf <- function(input_file, output_file = NULL,
 # Example usage:
 # tiff_to_netcdf("input.tif", decimals = 5, overwrite = TRUE)
 
-# Naming convention:
-# <scenario>_<group>_<species>_<info>_CORR_YEARLY.nc
-# group:   ITAWG
-# species: O3, PM25, NO2
-# info:    <sequence>.<calibration>.<correction>.<spatialization>
-scen_in <- "unbiased_basecase"
-scen_out <- "BaseCase"
-group <- "ITAWG"
-for (f in Sys.glob(glue("data/processed/*_{scen_in}.tif"))) {
-  specie <- strsplit(basename(f), "_")[[1]][1]
-  varname <- case_match(specie, "O3"~"SURF_ppb_O3", "PM25"~"SURF_ug_PM25_rh50", "NO2"~"SURF_ug_NO2")
-  unit <- case_match(specie, "O3"~"ppb", "PM25"~"ug/m3", "NO2"~"ug/m3")
-  info <- strsplit(basename(f), "_")[[1]][2]
-  infos <- strsplit(info,"\\.")[[1]]
-  if(length(infos)==4) infos[4] <- str_to_upper(infos[4])
-  fileout <- glue("data/fairmode-wg5-exercise-output/{scen_out}_{group}_{specie}_{paste(infos,collapse='.')}_CORR_YEARLY.nc")
-  tiff_to_netcdf(input_file = f, output_file = fileout, overwrite = T, varname = varname, unit = unit)
-  flog.info(glue("Written file {fileout}"))
+exercise <- "phase2"
+
+if(exercise=="phase1") {
+  #---------------------
+  # FAIRMODE WG5 Phase 1
+  # Naming convention:
+  # <scenario>_<group>_<species>_<info>_CORR_YEARLY.nc
+  # group:   ITAWG
+  # species: O3, PM25, NO2
+  # info:    <sequence>.<calibration>.<correction>.<spatialization>
+  scen_in <- "unbiased_basecase"
+  scen_out <- "BaseCase"
+  group <- "ITAWG"
+  for (f in Sys.glob(glue("data/processed_fairmode/*_{scen_in}.tif"))) {
+    specie <- strsplit(basename(f), "_")[[1]][1]
+    varname <- case_match(specie, "O3"~"SURF_ppb_O3", "PM25"~"SURF_ug_PM25_rh50", "NO2"~"SURF_ug_NO2")
+    unit <- case_match(specie, "O3"~"ppb", "PM25"~"ug/m3", "NO2"~"ug/m3")
+    info <- strsplit(basename(f), "_")[[1]][2]
+    infos <- strsplit(info,"\\.")[[1]]
+    if(length(infos)==4) infos[4] <- str_to_upper(infos[4])
+    fileout <- glue("data/fairmode-wg5-exercise-output/{scen_out}_{group}_{specie}_{paste(infos,collapse='.')}_CORR_YEARLY.nc")
+    tiff_to_netcdf(input_file = f, output_file = fileout, overwrite = T, varname = varname, unit = unit)
+    flog.info(glue("Written file {fileout}"))
+  }
+}
+
+if(exercise=="phase2") {
+  #---------------------
+  # FAIRMODE WG5 Phase 2
+  # Naming convention:
+  # Scen_<year>_<group>_<species>_<info>_CORR_YEARLY.nc
+  # group:   ITAWG
+  # species: O3, PM25, NO2
+  # info:    <sequence>.<calibration>.<correction>.<spatialization>
+  scen_in <- "unbiased_scenario"
+  dir_in <- "data/processed_phase2"
+  dir_out <- "data/fairmode-wg5-exercise-phase2-output"
+  group <- "ITAWG"
+  for (f in Sys.glob(glue("{dir_in}/*_{scen_in}_????.tif"))) {
+    specie <- strsplit(basename(f), "_")[[1]][1]
+    varname <- specie
+    unit <- case_match(specie, "O3"~"ug/m3", "PM25"~"ug/m3", "NO2"~"ug/m3")
+    info <- strsplit(basename(f), "_")[[1]][2]
+    infos <- strsplit(info,"\\.")[[1]]
+    year <- tools::file_path_sans_ext(strsplit(basename(f), "_")[[1]][5])
+    if(length(infos)==4) infos[4] <- str_to_upper(infos[4])
+    fileout <- glue("{dir_out}/Scen_{year}_{group}_{specie}_{paste(infos,collapse='.')}_CORR_YEARLY.nc")
+    tiff_to_netcdf(input_file = f, output_file = fileout, overwrite = T, varname = varname, unit = unit)
+    flog.info(glue("Written file {fileout}"))
+  }
 }
